1564d2b6bed8b867a011aacc2bfc1bb81d503dc5
Kimmo Juusola <c8juki00@students.oamk.fi>
2019-11-12 10:59:06 +0200

    added mysql connection

--- homeassistant.py
+++ homeassistant.py
@@ -1,21 +1,42 @@
 import serial
+import time
+import mysql.connector
 
+db_conn = mysql.connector.connect(
+  host = "localhost",
+  user = "root",
+  passwd = "test",
+  database = "test"
+)
 
-# this port address is for the serial tx/rx pins on the GPIO header
-SERIAL_PORT = 'COM6'
-# be sure to set this to the same rate used on the Arduino
-SERIAL_RATE = 9600
+db_stmt = db_conn.cursor()
 
+sql = "SELECT * FROM temperature"
+db_stmt.execute(sql)
+result = db_stmt.fetchone()
+print("result: " + str(result))
 
-def main():
-    ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
-    while True:
-        # using ser.readline() assumes each line contains a single reading
-        # sent using Serial.println() on the Arduino
-        reading = ser.readline().decode('utf-8')
-        # reading is a string...do whatever you want from here
-        print(reading)
+SERIAL_PORT = "COM12"
+SERIAL_BAUD = 9600
 
+while True:
+    try:
+        SERIAL_PORT = input("SerialPort: ")
+        SERIAL_BAUD = input("Baud Rate: ")
+        ser = serial.Serial(SERIAL_PORT, SERIAL_BAUD)
+        time.sleep(2)
+        print("Connected to SerialPort: " + str(SERIAL_PORT) + ", baud rate: " + str(SERIAL_BAUD))
 
-if __name__ == "__main__":
-    main()
+        while True:
+            read_serial = ser.readline().decode('utf-8') # Removing b, \r, \n from the line
+
+            if read_serial[:12] == "Temperature1":
+                current_temp = read_serial[14:]
+                print("Data from SerialPort: " + current_temp)
+                sql = "UPDATE temperature SET currentTemperature =  %s WHERE idTemperature = %s"
+                val = (current_temp, 1)
+                db_stmt.execute(sql, val)
+                db_conn.commit()
+                print(db_stmt.rowcount, "record(s) affected")
+    except:
+        print("Check connections.")
