5790ab3c1a24a0aeb9776f6a81457bb6e316005f
Kimmo Juusola <c8juki00@students.oamk.fi>
2019-11-26 12:21:41 +0200

    update serial read

--- homeassistant.py
+++ homeassistant.py
@@ -2,29 +2,29 @@ import serial
 import time
 import requests
 
-
 SERIAL_PORT = "COM12"
 SERIAL_BAUD = 9600
 
 
 def rTemperature(currentTemp):
     try:
-        rTemp = requests.post('http://ec2-35-158-176-134.eu-central-1.compute.amazonaws.com/temperature', json={'temperature': currentTemp})
-        print (rTemp.status_code)
+        rTemp = requests.post('http://ec2-35-158-176-134.eu-central-1.compute.amazonaws.com/temperature',
+                              json={'temperature': currentTemp})
+        print(rTemp.status_code)
 
     except requests.exceptions.MissingSchema:
         print("Invalid URL")
     finally:
         time.sleep(5)
 
+
 def rLights():
     rLights = requests.get('http://ec2-35-158-176-134.eu-central-1.compute.amazonaws.com/lights/1')
     print(rLights.status_code)
     data = rLights.json()
 
-    ser.write(data[0]['lightStatus'])
+    #ser.write(data[0]['lightStatus'])
     time.sleep(1)
-
 
 
 while True:
@@ -36,16 +36,17 @@ while True:
         print("Connected to SerialPort: " + str(SERIAL_PORT) + ", baud rate: " + str(SERIAL_BAUD))
 
         while True:
-            read_serial = ser.readline().decode('utf-8') # Removing b, \r, \n from the line
+            read_serial = ser.readline().decode('utf-8')  # Removing b, \r, \n from the line
 
             if read_serial[:12] == "Temperature1":
                 current_temp = read_serial[14:]
-                #print("Data from SerialPort: " + current_temp)
+                current_temp = current_temp.rstrip()
+                # print("Data from SerialPort: " + current_temp)
                 rTemperature(current_temp)
 
             rLights()
-                
-                
+
+
 
     except:
         print("Check connections.")
